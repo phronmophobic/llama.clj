fdd6898fa52b2f72588f01574a57dcd4168bd54b
Adrian Smith <smith.adriane@gmail.com>
2023-07-19 19:32:09 -0700

    Add workflow for deploying to clojars.

---
+++ build.clj
@@ -0,0 +1,45 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]
+            [clojure.string :as str]))
+
+(def lib 'com.phronemophobic/llama-clj)
+(def version "0.1.0")
+
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn compile [_]
+  #_(b/javac {:src-dirs ["src-java"]
+            :class-dir class-dir
+            :basis basis
+            :javac-opts ["-source" "8" "-target" "8"]}))
+
+(defn jar [opts]
+  (compile opts)
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
+
+(defn deploy [opts]
+  (jar opts)
+  (try ((requiring-resolve 'deps-deploy.deps-deploy/deploy)
+        (merge {:installer :remote
+                :artifact jar-file
+                :pom-file (b/pom-path {:lib lib :class-dir class-dir})}
+               opts))
+       (catch Exception e
+         (if-not (str/includes? (ex-message e) "redeploying non-snapshots is not allowed")
+           (throw e)
+           (println "This release was already deployed."))))
+  opts)
+
