6a4f6286572b292d22dc2582d31dd4eb6ad29718
Adrian Smith <smith.adriane@gmail.com>
2023-10-02 17:35:46 -0700

    update tests.

--- test/com/phronemophobic/llama/token_gguf_test.clj
+++ test/com/phronemophobic/llama/token_gguf_test.clj
@@ -1,4 +1,4 @@
-(ns com.phronemophobic.llama.token-test
+(ns com.phronemophobic.llama.token-gguf-test
   (:require [clojure.test :refer :all]
             [clojure.string :as str]
             [clojure.test.check.generators :as gen]
@@ -38,3 +38,27 @@
   (tc/quick-check 10000 (token-roundtrip ctx))
 
   ,)
+
+(defspec token-llama2-roundtrip-alphanumeric-spec
+  10000
+  (let [model-path "models/llama-2-7b-chat.Q4_0.gguf"]
+    (let [ctx (llama/create-context model-path {:n-gpu-layers 1})]
+      (token-roundtrip-alphanumeric ctx))))
+
+(defspec token-mistral-roundtrip-alphanumeric-spec
+  10000
+  (let [model-path "models/mistral-7b-instruct-v0.1.Q4_0.gguf"]
+    (let [ctx (llama/create-context model-path {:n-gpu-layers 1})]
+      (token-roundtrip-alphanumeric ctx))))
+
+(defspec token-llama2-roundtrip-spec
+  10000
+  (let [model-path "models/llama-2-7b-chat.Q4_0.gguf"]
+    (let [ctx (llama/create-context model-path {:n-gpu-layers 1})]
+      (token-roundtrip ctx))))
+
+(defspec token-mistral-roundtrip-spec
+  10000
+  (let [model-path "models/mistral-7b-instruct-v0.1.Q4_0.gguf"]
+    (let [ctx (llama/create-context model-path {:n-gpu-layers 1})]
+      (token-roundtrip ctx))))
