1cb3f46ceb26fba543f31f5f3d2cec19ce083e46
Adrian Smith <smith.adriane@gmail.com>
2023-10-02 17:24:39 -0700

    updating rcf.

--- src/com/phronemophobic/llama.clj
+++ src/com/phronemophobic/llama.clj
@@ -292,6 +292,7 @@
 
   ;; https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/resolve/main/mistral-7b-instruct-v0.1.Q4_0.gguf
   (def model-path "models/mistral-7b-instruct-v0.1.Q4_0.gguf")
+  (def model-path "models/llama-2-7b-chat.Q4_0.gguf")
 
   (def ctx (create-context model-path {:n-ctx 2048}))
 
