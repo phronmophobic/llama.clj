73f12876acc5b4c59b1cfe997475bca67fea7c26
Adrian Smith <smith.adriane@gmail.com>
2024-03-02 12:19:29 -0800

    try ggml+gguf in sanity check again.

--- notebooks/sanity_check.clj
+++ notebooks/sanity_check.clj
@@ -28,8 +28,8 @@
    (keep (fn [[k path opts]]
            (when (.exists (io/file path))
              [k (llama/create-context path opts)])))
-   [;; [:llama7b-ggml llama7b-path {:n-gpu-layers n-gpu-layers}]
-    ;; [:llama7b-ggml-embedding llama7b-path {:embedding true :n-gpu-layers n-gpu-layers}]
+   [[:llama7b-ggml llama7b-path {:n-gpu-layers n-gpu-layers}]
+    [:llama7b-ggml-embedding llama7b-path {:embedding true :n-gpu-layers n-gpu-layers}]
     [:llama7b-gguf llama7b-gguf-path {:n-gpu-layers n-gpu-layers}]
     [:llama7b-gguf-embedding llama7b-gguf-path {:n-gpu-layers n-gpu-layers :embedding true}]
     [:gemma gemma-2b-path {:n-gpu-layers n-gpu-layers}]
