c9e5307c67e5b681d48d6a3c43efdcaad356ebeb
Adrian Smith <smith.adriane@gmail.com>
2024-03-02 12:19:13 -0800

    change library loading options.

--- src/com/phronemophobic/llama/raw.clj
+++ src/com/phronemophobic/llama/raw.clj
@@ -35,10 +35,27 @@
             *out* w]
     (pr obj)))
 
+  ;; private static final int RTLD_LAZY         = 0x00001; /* Lazy function call binding.  */
+  ;;   private static final int RTLD_NOW          = 0x00002; /* Immediate function call binding.  */
+  ;;   private static final int RTLD_NOLOAD       = 0x00004; /* Do not load the object.  */
+;;   private static final int RTLD_DEEPBIND     = 0x00008; /* Use deep binding.  */
+ ;; int RTLD_MEMBER = 0x40000; // allows "lib.a(obj.o)" syntax
+;; suggestion: https://github.com/java-native-access/jna/issues/724#issuecomment-258937700
+;; options.put(Library.OPTION_OPEN_FLAGS,  RTLD_NOW | RTLD_DEEPBIND);
+;; defaults: https://github.com/java-native-access/jna/blob/83f2fc875e736308f418e8e99ff714ec054d8af4/contrib/platform/src/com/sun/jna/platform/unix/aix/SharedObjectLoader.java#L59C9-L59C87
+;; options.put(Library.OPTION_OPEN_FLAGS, RTLD_MEMBER | RTLD_GLOBAL | RTLD_LAZY);
+(def RTLD_LOCAL 0)
+(def RTLD_MEMBER 0x40000)
+(def RTLD_LAZY 0x00001)
+
 (def
   ^java.util.Map
   libllama-options
-  {com.sun.jna.Library/OPTION_STRING_ENCODING "UTF8"})
+  {com.sun.jna.Library/OPTION_STRING_ENCODING "UTF8"
+   com.sun.jna.Library/OPTION_OPEN_FLAGS (bit-or
+                                          RTLD_LOCAL
+                                          RTLD_MEMBER
+                                          RTLD_LAZY)})
 (def ^:no-doc libllama
   (com.sun.jna.NativeLibrary/getInstance "llama" libllama-options))
 
