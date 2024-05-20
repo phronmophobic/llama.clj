b4f69f79de910afc880c77eb08d8ab45a1409d7e
Adrian Smith <smith.adriane@gmail.com>
2024-05-20 15:33:10 -0700

    bump version to 0.8.3.

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
             [clojure.string :as str]))
 
 (def lib 'com.phronemophobic/llama-clj)
-(def version "0.8.2")
+(def version "0.8.3")
 
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
